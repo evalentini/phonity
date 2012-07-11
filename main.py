929a01c90885e4a94a89dece5abb70c709d2a4f3
moose <moose@moose.(none)>
2012-07-10 22:51:31 -0500

    synced with most recent version deployed on app engine

--- main.py
+++ main.py
@@ -1,72 +1,24 @@
-
-
-#imports#####################################################################################################
-#############################################################################################################
-
-#import library for sending SMS through google voice
 from voice import Voice
 from util import input 
-#import jinja2 template library 
-import jinja2
-
-#import python standard distribution libraries 
+#import sms
 import webapp2
 import settings
 import os
-import re
 
-##configure jinja2 templates#################################################################################
-#############################################################################################################
-
-template_dir=os.path.join(os.path.dirname(__file__), 'templates')
-jinja2_env=jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
-
-##define helper functions###################################################################################
-############################################################################################################
-
-def render(template, **params):
-	t=jinja2_env.get_template(template)
-	return t.render(params)
-
-def text(message=None, number=None, email="evan.valentini@gmail.com", pwd="robots25"):
-	voice=Voice()
-	voice.login(email, pwd)
-	voice.send_sms(number, message)
-	
 VALID_PWD="shabanaSnuggle"
 
-
-###SIGN UP PAGE HANDLER###################################################################################################################################################################################
-
-class Signup(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write(render("signup.html"))
-	
-	def post(self):
-		#pull phone number entered
-		phone_number=self.request.get("phone_number")
-		admin_pwd=self.request.cookies.get("admin_pwd")
-		pwd_check= admin_pwd==VALID_PWD
-		#confirm admin password
-		if pwd_check==False:
-			self.redirect('/login') 
-		#regex verify phone number  
-		#should be 10 digits
-		PHONE_RE=re.compile(r'[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-		if PHONE_RE.match(phone_number):
-			#generate random pin
-			pin="1592"
-			#text pin to entered number
-			nil=text("Your pin is "+pin, phone_number) 
-		else:
-			self.response.out.write(render("signup.html"))
-			#add error message later	
-				
-
 class Login(webapp2.RequestHandler):
-
 	def get(self):
-		self.response.out.write(render("login.html"))
+		login_screen="""
+			<html>
+			<body>
+			<form method="post">
+				<input type="password" name="admin_pwd">
+				<input type="submit" value="login">
+			</form>
+			</body>
+			</html>"""
+		self.response.out.write(login_screen)
 
 	def post(self):
 		admin_pwd=self.request.get("admin_pwd")
@@ -107,8 +59,7 @@ class Welcome(webapp2.RequestHandler):
 			self.redirect('/login')
 
 app=webapp2.WSGIApplication([('/', Welcome),
-				('/login', Login),
-				('/signup', Signup)
+				('/login', Login)
 				], debug=True)
 
 
